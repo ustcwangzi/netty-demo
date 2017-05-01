9f3fb4519894a19faab4d2a8063c5f756d22ad09
wangzi <wangzi>
2017-05-01 15:53:56 +0800

    netty

---
+++ src/main/java/com/wz/netty/basic/NettyClient.java
@@ -0,0 +1,43 @@
+package com.wz.netty.basic;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by wangzi on 2017/5/1.
+ */
+public class NettyClient {
+    private static final int port = 8080;
+
+    public void connect(int port, String host) throws Exception {
+        // 配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class).option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyClientHandler());
+                        }
+                    });
+            // 发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+            // 当代客户端链路关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        new NettyClient().connect(port, "127.0.0.1");
+    }
+}
