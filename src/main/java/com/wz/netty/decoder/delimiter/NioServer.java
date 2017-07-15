8a875ff8a1d599968506841d3f50609178778afd
wangzi <wangzi>
2017-07-15 09:19:39 +0800

    DelimiterBasedFrameDecoder

---
+++ src/main/java/com/wz/netty/decoder/delimiter/NioServer.java
@@ -0,0 +1,56 @@
+package com.wz.netty.decoder.delimiter;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * Created by wangzi on 2017/5/3.
+ */
+public class NioServer {
+    private static final int port = 8080;
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            // 绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            // 等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 优雅退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            //分隔符缓冲对象
+            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+            ch.pipeline().addLast(new StringDecoder());
+            ch.pipeline().addLast(new NioServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        new NioServer().bind(port);
+    }
+}
---
+++ src/main/java/com/wz/netty/decoder/delimiter/NioServerHandler.java
@@ -0,0 +1,28 @@
+package com.wz.netty.decoder.delimiter;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * 解决TCP粘包/拆包问题
+ * Created by wangzi on 2017/5/3.
+ */
+public class NioServerHandler extends ChannelHandlerAdapter{
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("Receive : " + body + " ; the counter is : " + ++counter);
+        body += "$_";
+        ByteBuf resp = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
